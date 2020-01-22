5b2821e188b95a5a2ed72a95edb84f1fa6f215de
vasilio13 <vasilkhaliaukin@gmail.com>
2020-01-22 20:54:31 +0300

    bank begin

---
+++ src/Bank.java
@@ -0,0 +1,11 @@
+public class Bank {
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/bank/?serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) {
+        
+    }
+}
